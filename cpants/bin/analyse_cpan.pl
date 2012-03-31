f567e585e83a67c92bd75e14625054f1aa717d9f
Kenichi Ishigaki <ishigaki@cpan.org>
2012-03-31 11:38:27 +0900

    try to find installed cpants_lint.pl if not specified

--- cpants/bin/analyse_cpan.pl
+++ cpants/bin/analyse_cpan.pl
@@ -4,9 +4,12 @@ use warnings;
 use Module::CPANTS::ProcessCPAN;
 use Getopt::Long;
 use File::Spec::Functions qw(rel2abs);
+use File::Which;
 
 my %opts;
 GetOptions(\%opts,qw(cpan=s lint=s force));
+
+$opts{lint} ||= which('cpants_lint.pl');
 
 die "Usage: analyse_cpan.pl --cpan path/to/minicpan --lint path/to/cpants_lint.pl" unless $opts{cpan} && $opts{lint};
 die "Cannot find cpants_lint.pl (in ".$opts{lint}.")" unless -e $opts{lint};
